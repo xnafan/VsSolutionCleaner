6712f327a5e92803c05a9234c68a0a4eccebe408
Jakob Farian Krarup <kr@rup.dk>
2023-03-01 12:11:21 +0100

    Add project files.

---
+++ RecursiveDirectoryCleanerTool/CleanerTool.cs
@@ -0,0 +1,43 @@
+﻿namespace RecursiveDirectoryCleanerTool;
+public static class CleanerTool
+{
+
+    private readonly static string[] DirectoriesToClean = new string[] { "obj", "bin" };
+    public static void CleanDirectory(string directoryPath)
+    {
+        ValidateDirectoryExistenceAndThrowIfNotExists(directoryPath);
+        CleanDirectoryRecursive(directoryPath);
+    }
+
+
+    private static void ValidateDirectoryExistenceAndThrowIfNotExists(string directoryPath)
+    {
+        if (!Directory.Exists(directoryPath))
+        {
+            throw new DirectoryNotFoundException($"Directory {directoryPath} does not exist.");
+        }
+    }
+    private static void CleanDirectoryRecursive(string directoryPath)
+    {
+        var directoriesFound = Directory.GetDirectories(directoryPath);
+        foreach (var directory in directoriesFound)
+        {
+            if (!DeleteIfObjOrBin(directory))
+            {
+                CleanDirectoryRecursive(directory);
+            }
+        }
+    }
+
+    private static bool DeleteIfObjOrBin(string directory)
+    {
+        string directoryName = Path.GetFileName(directory).ToLower();
+
+        if (DirectoriesToClean.Contains(directoryName))
+        {
+            Directory.Delete(directory, true);
+            return true;
+        }
+        return false;
+    }
+}
---
+++ SolutionCleanerConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using RecursiveDirectoryCleanerTool;
+namespace SolutionCleanerConsoleApp;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        ValidateArgumentsAndExitIfInvalid(args);
+        CleanerTool.CleanDirectory(args[0]);
+    }
+
+    private static void ValidateArgumentsAndExitIfInvalid(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("You need to have exactly one (1) command line argument after this program on the command line.");
+            
+            WriteUsageAndExit();
+        }
+
+        if (!Directory.Exists(args[0]))
+        {
+            Console.WriteLine($"'{args[0]}' is not a directory!");
+            WriteUsageAndExit();
+        }
+    }
+
+    private static void WriteUsageAndExit()
+    {
+        WriteUsage();
+        Environment.Exit(-1);
+    }
+
+    private static void WriteUsage()
+    {
+        Console.WriteLine();
+        Console.WriteLine("VS Solution Cleaner Tool");
+        Console.WriteLine("------------------------");
+        Console.WriteLine("Usage: solutioncleaner.exe directory-to-clean");
+        Console.WriteLine(@"E.g. 'solutioncleaner.exe c:\users\me\code\myproject");
+    }
+}
